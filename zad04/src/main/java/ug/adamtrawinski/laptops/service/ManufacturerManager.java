341c9494dd17e9d0d3707056ea6d8fa56faca1a7
Adam Trawiński <adam.trawinski@poczta.fm>
2018-12-20 15:19:33 +0100

    Operacje dla producenta

---
+++ zad04/src/main/java/ug/adamtrawinski/laptops/service/ManufacturerManager.java
@@ -0,0 +1,14 @@
+package ug.adamtrawinski.laptops.service;
+
+import ug.adamtrawinski.laptops.domain.Manufacturer;
+
+import java.util.List;
+
+public interface ManufacturerManager {
+    void addManufacturer(Manufacturer manufacturer);
+    Manufacturer findManufacturerById(long id);
+    List<Manufacturer> getAllManufacturers();
+    void deleteManufacturer(long id);
+    void updateManufacturer(Manufacturer laptop);
+    void clearTable();
+}
---
+++ zad04/src/main/java/ug/adamtrawinski/laptops/service/ManufacturerMangerHibernateImpl.java
@@ -0,0 +1,55 @@
+package ug.adamtrawinski.laptops.service;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ug.adamtrawinski.laptops.domain.Manufacturer;
+
+import java.util.List;
+
+@Component
+@Transactional
+public class ManufacturerMangerHibernateImpl implements ManufacturerManager {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+
+	@Override
+	public void addManufacturer(Manufacturer manufacturer) {
+		sessionFactory.getCurrentSession().persist(manufacturer);
+	}
+
+	@Override
+	public Manufacturer findManufacturerById(long id) {
+		return (Manufacturer) sessionFactory.getCurrentSession().get(Manufacturer.class, id);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<Manufacturer> getAllManufacturers() {
+		return sessionFactory.getCurrentSession().createCriteria(Manufacturer.class).list();
+	}
+
+	@Override
+	public void deleteManufacturer(long id) {
+		Manufacturer manufacturer = findManufacturerById(id);
+		sessionFactory.getCurrentSession().delete(manufacturer);
+	}
+
+	@Override
+	public void updateManufacturer(Manufacturer manufacturer) {
+		Manufacturer old = findManufacturerById(manufacturer.getId());
+		if(old != null) {
+			sessionFactory.getCurrentSession().merge(manufacturer);
+		}
+	}
+
+	@Override
+	public void clearTable() {
+		sessionFactory.getCurrentSession().createSQLQuery("TRUNCATE TABLE MANUFACTURER RESTART IDENTITY").executeUpdate();
+	}
+
+
+}
